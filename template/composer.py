bed7f64752bb45031671c4307d9ec4ee691bb071
dekunle02 <dekunle02@gmail.com>
2022-01-24 11:23:23 +0000

    attempt2

--- template/composer.py
+++ template/composer.py
@@ -7,11 +7,12 @@ from pathlib import Path
 import pdfkit
 
 TEMPLATE_DIRECTORY = Path(__file__).parents[0] / 'template.html'
-
 today = str(date.today().strftime('%d-%m-%Y'))
+
+OUTPUT_FILENAME = 'timesheet'
 OUT_DIR = Path(__file__).parents[1] / 'out'
-OUT_PDF = Path(__file__).parents[1] / 'out' / (today + '.pdf')
-OUT_HTML = Path(__file__).parents[1] / 'out' / (today + '.html')
+OUT_PNG = OUT_DIR / (OUTPUT_FILENAME + '.png')
+OUT_HTML = OUT_DIR / (OUTPUT_FILENAME + '.html')
 
 LOGO_DIR = Path(__file__).parents[1] / 'res' / 'nes_logo.jpg'
 SIGNATURE_DIRECTORY = Path(__file__).parents[1] / 'database' / 'signature.jpg'
@@ -56,11 +57,6 @@ def write_bio(text: str, timesheet: TimeSheet) -> str:
     }
     return replace_all(text, replace_dic)
 
-
-def write_date(text: str, timesheet=None) -> str:
-    return text.replace('XXTODAY_DATEXX', today)
-
-
 def write_disturbances(text: str, timesheet: TimeSheet) -> str:
     disturbances = timesheet.data['night_disturbances']
     counter = 1
@@ -80,6 +76,7 @@ def write_hours(text: str, timesheet: TimeSheet) -> str:
     table_data = timesheet.data['table_data']
     entries = table_data['entries']
     entry_strings = ""
+    last_date = ''
     for entry in entries:
         entry_weekday = WEEKDAYS[entry['date'].weekday()]
         entry_date = entry['date'].strftime('%d-%m-%Y')
@@ -91,11 +88,13 @@ def write_hours(text: str, timesheet: TimeSheet) -> str:
                        f"<td>{entry['hours']}</td>\n" \
                        f"</tr>\n"
         entry_strings += entry_string
+        last_date = entry_date
+    
     template_dic = {
         'XXENTRIESXX': entry_strings,
         'XXTOTAL_WEEK_HOURSXX': table_data['total_hours']
     }
-    text = replace_all(text, template_dic)
+    text = replace_all(text, template_dic).replace('XXTODAY_DATEXX', last_date)
     return text
 
 
@@ -120,33 +119,12 @@ def compose(timesheet: TimeSheet, text: str, *args):
 
 def produce(timesheet: TimeSheet):
     text = obtain_sheet()
-
     composition = compose(timesheet, text,
-                          write_css_n_images, write_bio, write_date, write_disturbances, write_hours, clean_tags)
+                          write_css_n_images, write_bio, write_disturbances, write_hours, clean_tags)
 
     if not OUT_DIR.exists():
         OUT_DIR.mkdir(parents=True, exist_ok=True)
-
+        
     with codecs.open(str(OUT_HTML), 'w') as f:
         f.write(composition)
-    options = {
-        "enable-local-file-access": None,
-        "page-size": 'A4',
-        'margin-top': '0in',
-        'margin-right': '0in',
-        'margin-bottom': '0in',
-        'margin-left': '0in',
-        'orientation': 'landscape'
-    }
-    css = [str(BOOTSTRAP_DIR), str(STYLES_DIR)]
-    pdfkit.from_file(str(OUT_HTML), str(OUT_PDF), options=options, css=css)
-
-# if __name__ == '__main__':
-#     sample_user = User(first_name="Samad", last_name="Adeleke", hospital_name="Nuffield")
-#     start_date = '25/10/2021'
-#     end_date = '1/11/2021'
-#     start_time = '13:00'
-#     end_time = '13:00'
-#
-#     t = TimeSheet(sample_user, start_date, start_time, 7, end_time)
-#     produce(t)
+    
